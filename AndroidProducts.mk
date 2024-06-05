2479dfc9911a74ea10d4e42aa1cc7f33dcd409d8
Ricky Souza de Oliveira <38869320+HeroRickyGAMES@users.noreply.github.com>
2024-06-05 01:22:56 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beryllium.mk
+    $(LOCAL_DIR)/miku_beryllium.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beryllium-user \
-    lineage_beryllium-userdebug \
-    lineage_beryllium-eng
+    miku_beryllium-up1a-user \
+    miku_beryllium-up1a-userdebug \
+    miku_beryllium-up1a-eng
